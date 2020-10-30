4d5c0bda08b20d3890ac16b1056569c00547bd62
AnhNT <timer217@gmail.com>
2020-10-30 14:51:14 +0700

    update

--- ASP.NET MVC Training/User Manager/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="User_Manager.MvcApplication" Language="C#" %>
---
+++ DOTNET TRAINING/DOTNET TRAINING/Controllers/UserController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DOTNET_TRAINING.Models;
+using PagedList;
+
+namespace DOTNET_TRAINING.Controllers
+{
+    public class UserController : Controller
+    {
+        VietISEntities db = new VietISEntities();
+
+        //Get: User/Index
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 10;
+            int pageIndex = (page ?? 1);
+            return View(db.Users.ToList().ToPagedList(pageIndex, pageSize));
+        }
+
+        //Get: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Post: User/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "ID,FirstName,LastName,Birthday,Email,PhoneNumber")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        //Get: User/Edit/id
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        //Post: UserTest/Edit/id
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,Birthday,Email,PhoneNumber")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        //Get: UserTest/Delete/id
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        //Post: UserTest/Delete/id
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
